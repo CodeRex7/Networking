f0d5ba8719be9fe6d8bb3f7d3495e2812abb7408
sonicaks <adityakumarsinha2014@gmail.com>
2018-09-07 11:25:30 +0530

    Implemented Stop N Wait protocol

---
+++ lab-3/Stop N Wait.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <pthread.h>
+#include <unistd.h>
+
+using namespace std;
+
+int ack[15], frame[15];
+
+void *send(void *args) {
+    int id = (*(int *)args);
+    while (!ack[id]) {
+        cout << "Sending frame: " << id << "\n";
+        frame[id] = 1;
+        usleep(2000000);
+    }
+    pthread_exit(NULL);
+}
+
+void *receive(void *args) {
+    int id = (*(int *)args);
+    while (!frame[id]);
+    // if (id == 2) usleep(5000000);
+    cout << "Acknowledged frame: " << id << "\n";
+    ack[id] = 1;
+    pthread_exit(NULL);
+}
+
+int main() {
+    int totFrames = rand() % 5 + 1;
+    cout << "Total frames to send: " << totFrames << "\n";
+    pthread_t sendThread, receiveThread;
+    for (int i = 1; i <= totFrames; i++) {
+        int sendOk = pthread_create(&sendThread, NULL, send, (void *)&i);
+        if (sendOk) {
+            cout << "Error while sending frame: " << i << "\n";
+            exit(0);
+        }
+        int receiveOk = pthread_create(&receiveThread, NULL, receive, (void *)&i);
+        if (receiveOk) {
+            cout << "Error while receiving frame: " << i << "\n";
+            exit(0);
+        }
+        pthread_join(sendThread, NULL);
+        pthread_join(receiveThread, NULL);
+    }
+    pthread_exit(NULL);
+    return 0;
+}

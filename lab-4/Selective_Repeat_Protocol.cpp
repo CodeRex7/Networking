cd287c69d38896058c149efca2b5ec7f44cefeb6
sonicaks <adityakumarsinha2014@gmail.com>
2018-10-05 14:50:12 +0530

    Implemented Selective Repeat error control protocol

---
+++ lab-4/Selective_Repeat_Protocol.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <pthread.h>
+#include <unistd.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> frames;
+int NACK = -1, ok = 0;
+int error[35];
+
+void *send(void *args) {
+    int numFrames = (*(int *)args);
+
+    for (int i = 0; !ok; ) {
+        if (NACK != -1) {
+            cout << "Resent frame number:\t" << NACK << "\n";
+            frames.push_back(NACK);
+            NACK = -1;
+        } else if (i < numFrames) {
+            cout << "Sent frame number:\t" << i << "\n";
+            frames.push_back(i);
+            i++;
+        }
+
+        usleep(1000000);
+    }
+
+    pthread_exit(NULL);
+}
+
+void *recv(void *args) {
+    int numFrames = (*(int *)args);
+    int processdFrames = 0;
+
+    while (1) {
+        if (!frames.empty()) {
+            cout << "Received frame number:\t" << frames[0] << "\n";
+            processdFrames++;
+        }
+
+        if (!frames.empty() and error[frames[0]]) {
+            cout << "Erroneous frame number:\t" << frames[0] << "\n";
+            processdFrames--;
+            NACK = frames[0];
+            frames.erase(frames.begin());
+            while (NACK != -1);
+            error[frames[0]] = 0;
+            continue ;
+        }
+
+        if (!frames.empty()) {
+            sort(frames.begin(), frames.end());
+            frames.erase(frames.begin());
+        }
+        
+        if (processdFrames == numFrames) break ;
+
+        usleep(1555555);
+    }
+
+    ok = 1;
+
+    pthread_exit(NULL);
+}
+
+int main() {
+    int numFrames;
+    cout << "Enter number of frames to send: ";
+    cin >> numFrames;
+
+    error[5] = error[8] = error[13] = error[19] = 1;
+
+    pthread_t sendThread, recvThread;
+
+    int sendStatus = pthread_create(&sendThread, NULL, send, (void *)&numFrames);
+    int recvStatus = pthread_create(&recvThread, NULL, recv, (void *)&numFrames);
+
+    pthread_join(sendThread, NULL);
+    pthread_join(recvThread, NULL);
+    pthread_exit(NULL);
+
+    return 0;
+}
